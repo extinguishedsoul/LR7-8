49751a1fa9774a5ab4c6f57e1bf8c3d37b0c6bf9
extinguishedsoul <vilordimarianna343@gmail.com>
2024-12-04 16:25:04 +0300

    Add files via upload

---
+++ lbrtrn5tsk3.pas
@@ -0,0 +1,21 @@
+﻿program lbrtrn5tsk3;
+var mssv: array[1..20] of integer; i, j, nwrzmr: integer; prdchnmssv: array[1..20] of integer;
+begin
+  for i := 1 to 20 do
+  begin
+    write('Введите элемент ', i, ': ');
+    readln(mssv[i]);
+  end;
+  nwrzmr := 0; 
+  for i := 1 to 20 do
+  begin
+    if mssv[i] >= 0 then
+    begin
+      nwrzmr := nwrzmr + 1; 
+      prdchnmssv[nwrzmr] := mssv[i]; 
+    end;
+  end;
+  writeln('Массив без отрицательных элементов:');
+  for i := 1 to nwrzmr do
+    write(prdchnmssv[i], ' ');
+end.
