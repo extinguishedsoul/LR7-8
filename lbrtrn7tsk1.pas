dd4197e2cf0d8b1528c2fa0d8ebbd52e7152f525
extinguishedsoul <vilordimarianna343@gmail.com>
2024-12-04 16:23:18 +0300

    Add files via upload

---
+++ lbrtrn7tsk1.pas
@@ -0,0 +1,13 @@
+﻿program lbrtn7tsk1;
+var txt: string;
+begin
+    writeln('Введите свой текст:');
+    readln(txt);
+    while pos('Nikolay', txt) > 0 do
+    begin
+        delete(txt, pos('Nikolay', txt), length('Nikolay'));
+        insert('Oleg', txt, pos('Nikolay', txt));
+    end;
+    writeln('Измененный текст: ');
+    writeln(txt);
+end.
