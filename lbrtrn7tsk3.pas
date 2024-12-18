33e267a3d87638e12d3123ffc5ffe5e1e1ed7271
extinguishedsoul <vilordimarianna343@gmail.com>
2024-12-18 21:31:35 +0300

    Add files via upload

---
+++ lbrtrn7tsk3.pas
@@ -0,0 +1,19 @@
+﻿program lbrtrn7tsk3;
+var strk: string; i: integer; krktnst: boolean;
+begin
+    writeln('Введите строку:');
+    readln(strk);
+    krktnst := true;
+    for i := 1 to length(strk) do
+    begin
+        if not (strk[i] in ['a', 'b', 'c']) then
+        begin
+            krktnst := false;
+            break;
+        end;
+    end;
+    if krktnst then
+        writeln('Строка содержит только символы a, b, c')
+    else
+        writeln('Строка содержит символы помимо a, b, c');
+end.
